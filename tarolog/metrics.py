5f66f837b2e1b5263f89f832c95965c0b989039d
Anton Kurenkov <quantum0@mail.ru>
2023-09-12 22:16:06 +0300

    WiP Add metrics

---
+++ tarolog/metrics.py
@@ -0,0 +1,34 @@
+import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String, create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
+
+engine = create_engine("sqlite:///metrics.db")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Metrics(Base):
+    __tablename__ = "metrics"
+
+    username = Column(String, primary_key=True)
+    interactions_count = Column(Integer, default=0)
+    first_action = Column(DateTime, default_factory=datetime.datetime.utcnow)
+    last_action = Column(DateTime, default_factory=datetime.datetime.utcnow)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def update_user(username: str):
+    with Session(autoflush=False, bind=engine) as db:
+        user: Metrics | None = db.get(Metrics, username)
+        if user is None:
+            user = Metrics()
+            user.username = username
+        user.last_action = datetime.datetime.utcnow()
+        user.interactions_count += 1
+
+        db.commit()
